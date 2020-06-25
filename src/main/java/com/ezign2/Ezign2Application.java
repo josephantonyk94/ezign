b25832cc0c9638c10e6afbd9c42735ed86115001
josephantonyk94 <josephantonyk94@gmail.com>
2020-06-25 20:05:05 +0530

    ezign first commit
    to masrter

---
+++ src/main/java/com/ezign2/Ezign2Application.java
@@ -0,0 +1,13 @@
+package com.ezign2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ezign2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ezign2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezign2/Ezign2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezign2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ezign2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
